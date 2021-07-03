e2e99823bcf9a44151f388ed6617d720276b8014
Derrick A <derrickcanbereached@gmail.com>
2021-07-03 07:50:02 -0400

    refactor timer to use Date.now and fix conversion of ms

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let time = 0
+//Variables
+let start 
+let now 
+let elapsed 
 
-let lapNumber = 0
+let lapNumber = 0 
+let laps = []
+
 let state
 let interval
 
+//DOM Mapping/Setup
 const startButton = document.getElementById('start')
 const stopButton = document.getElementById('stop')
 const lapButton = document.getElementById('lap')
@@ -12,90 +18,117 @@ const displayElement = document.getElementById('display')
 const lapElement = document.getElementById('laps')
 
 displayElement.innerText = convert(0)
-lapButton.disable = true
+lapButton.disabled = true;
+stopButton.disabled = true;
 
+//Stopwatch Event Listeners
 startButton.addEventListener('click', () => {
-    state = true
-    interval = setInterval(timer, 1000)
-    startButton.disabled = true
-    lapButton.disabled = false
+    if(!start) {
+        start = Date.now();
+    };
+    state = true;
+    interval = setInterval(timer, 1);
+    startButton.disabled = true;
+    lapButton.disabled = false;
+    stopButton.disabled = false;
 })
+
 stopButton.addEventListener('click', () => {
-    state = null
-    interval = null
-    startButton.disabled = false
-    lapButton.disable = true
+    state = null;
+    interval = null;
+    startButton.disabled = false;
+    lapButton.disabled = true;
+    stopButton.disabled = true;
 })
 
 lapButton.addEventListener('click', () => {
-    lap = convert(time)
-    time = 0
-    let newLap = lapMaker(lap)
-    console.log(newLap)
-    lapElement.appendChild(newLap)
+    if(!state) return;
+    lap = Date.now();
+    let lapTime;    
+    laps.push(lap);
+    if(laps.length < 2) {
+        lapTime = lap - start
+    } else {
+        lapTime = laps[laps.length - 1] - 
+        laps[laps.length - 2]
+    };
+    let newLap = lapMaker(lapTime);
+    lapElement.appendChild(newLap);
 })
-
 
 resetButton.addEventListener('click', () => {
-    time = 0
-    interval = null
-    state = null
-    displayElement.innerText = convert(0)
-    startButton.disabled = false
-    lapButton.disable = true
+    reset()
+});
+
+
+//Helper Functions
+function timer() {
+    if(state) {
+        now = Date.now();
+        elapsed = now - start;
+        displayElement.innerText = convert(elapsed);
+    };
+};
+
+function reset() {
+    start = null;
+    interval = null;
+    state = null;
+    lapNumber = 0;
+    laps = [];
+    displayElement.innerText = convert(0);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    lapButton.disabled = true;
     while(lapElement.firstChild) {
         lapElement.removeChild(lapElement.firstChild)
-    }
-})
-
-
-//functions
-function timer() {
-    while(state) {
-        time++
-        let output = convert(time)
-        displayElement.innerText = output
-        return time
-    }
+    };
 }
 
 function convert(ms) {
-    let h = 0
-    let m = 0
-    let s = ms
-    while(s >= 1000) {
+    let m = 0;
+    let s = 0;
+    let h = 0;
+    while(ms >= 1000) {
+        ms -= 1000
+        s += 1
+    };
+    while(s >= 60) {
         s -= 60
         m += 1
-    }
+    };
     while(m >= 60) {
         m -= 60
         h += 1
-    }
-    let output = format([h, m, s, ms])
-    return output
-}
+    };
+    return format([h, m, s, ms]);
+};
 
 function format(arr) {
-    let output = []
+    let output = [];
     arr.forEach(value => {
-        let str = value.toString()
+        let str = value.toString();
         if(str.length < 2) {
             output.push(0 + str)
         } else {
             output.push(str)
-        }
-    })
-    return `${output[0]}:${output[1]}:${output[2]}`
+        };
+    });
+    while(output[3].length < 3) {
+        let ms = 0 + output[3]
+        output[3] = ms
+    }
+    return `${output[0]}:${output[1]}:${output[2]}.${output[3]}`;
 }
 
 function lapMaker(elapsedTime) {
-    let article = document.createElement('article')
-    let lap = document.createElement('p')
-    let time = document.createElement('p')
-    lapNumber++
-    time.innerText = elapsedTime
-    lap.innerText = `Lap #${lapNumber}:`
-    article.appendChild(lap)
-    article.appendChild(time)
-    return article
+    let article = document.createElement('article');
+    let lap = document.createElement('p');
+    let elapsed = document.createElement('p');
+    lapNumber++;
+    elapsed.innerText = convert(elapsedTime);
+    lap.innerText = `Lap #${lapNumber}:`;
+    article.appendChild(lap);
+    article.appendChild(elapsed);
+    return article;
 }
